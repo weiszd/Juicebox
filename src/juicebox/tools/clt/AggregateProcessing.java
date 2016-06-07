d0010fbe178c493d75e7236c1b5f0f901d066c4f
Muhammad S Shamim <sa501428@gmail.com>
2016-06-07 16:39:38 -0500

    APA updates APA corner region width customization added

--- src/juicebox/tools/clt/AggregateProcessing.java
+++ src/juicebox/tools/clt/AggregateProcessing.java
@@ -24,15 +24,26 @@
 
 package juicebox.tools.clt;
 
-import jargs.gnu.CmdLineParser;
-import juicebox.HiCGlobals;
-import juicebox.tools.utils.juicer.hiccups.HiCCUPSUtils;
-import juicebox.track.feature.Feature2DList;
-import juicebox.track.feature.Feature2DParser;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.mahout.clustering.Cluster;
+import org.apache.mahout.clustering.classify.WeightedVectorWritable;
+import org.apache.mahout.clustering.kmeans.KMeansDriver;
+import org.apache.mahout.clustering.kmeans.Kluster;
+import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created for testing multiple CLTs at once
@@ -40,95 +51,148 @@ import java.util.Map;
  */
 class AggregateProcessing {
 
-
+/*
     public static void main(String[] argv) throws IOException, CmdLineParser.UnknownOptionException, CmdLineParser.IllegalOptionValueException {
 
-        /*
-        /Users/muhammadsaadshamim/Desktop/test_motifs/original
-muhammads-mbp:original muhammadsaadshamim$ ls
-geo.txt		java_motifs.txt
-         */
+        //input data
+        double[][] points = { {1, 1}, {2, 1}, {1, 2}, {2, 2}, {3, 3}, {8, 8},{9, 8}, {8, 9}, {9, 9} };
 
-        HiCGlobals.printVerboseComments = true;
+        FileSystem fs = new RawLocalFileSystem();
+        Configuration conf = fs.getConf();
 
-        Map<Integer, Feature2DList> map = new HashMap<Integer, Feature2DList>();
-        map.put(5000, Feature2DParser.loadFeatures("/Users/muhammadsaadshamim/Desktop/T0_48/48/exp1/postprocessed_pixels_5000"
-                , "hg19", true, null, false));
-        map.put(10000, Feature2DParser.loadFeatures("/Users/muhammadsaadshamim/Desktop/T0_48/48/exp1/postprocessed_pixels_10000"
-                , "hg19", true, null, false));
-        map.put(25000, Feature2DParser.loadFeatures("/Users/muhammadsaadshamim/Desktop/T0_48/48/exp1/postprocessed_pixels_25000"
-                , "hg19", true, null, false));
+        //vectorize the input data and prepare the input files.
+        List<Vector> vectors = new ArrayList<Vector>();
+        for (int i = 0; i < points.length; i++) {
+            double[] fr = points[i];
+            Vector vec = new NamedVector(new RandomAccessSparseVector(fr.length), "point"+ i);
+            vec.assign(fr);
+            vectors.add(vec);
+        }
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, new Path("clustering/testdata/points/file1"),
+                LongWritable.class, VectorWritable.class);
+        long recNum = 0;
+        VectorWritable vw = new VectorWritable();
+        for (Vector point : vectors) {
+            vw.set(point);
+            writer.append(new LongWritable(recNum++), vw);
+        }
+        writer.close();
 
-        Feature2DList newMerger = HiCCUPSUtils.mergeAllResolutions(map);
+        //choose two random clusters to begin with
+        Path path = new Path("clustering/testdata/clusters/part-00000");
+        writer = new SequenceFile.Writer(fs, conf, path, Text.class, Kluster.class);
+        int k = 2;
+        for (int i = 0; i < k; i++) {
+            Vector vec = vectors.get(7 - i);
+            Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
+            writer.append(new Text(cluster.getIdentifier()), cluster);
+        }
+        writer.close();
 
-       // newMerger.exportFeatureList("/Users/muhammadsaadshamim/Desktop/T0_48/48/exp1/new_merged_loops", false, Feature2DList.ListFormat.FINAL);
+        //run the k-means algorithm
+        try {
+            KMeansDriver.run(conf,
+                    new Path("clustering/testdata/points"),
+                    new Path("clustering/testdata/clusters"),
+                    new Path("clustering/output"),
+                    0.001, 10, true, 0.001, true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs,
+                new Path("clustering/output/" + Cluster.CLUSTERED_POINTS_DIR + "/part-m-0"), conf);
+
+        IntWritable key = new IntWritable();
+        WeightedPropertyVectorWritable value = new WeightedPropertyVectorWritable();
+        while (reader.next(key, value)) {
+            System.out.println(value.toString() + " belongs to cluster " + key.toString());
+        }
+        reader.close();
+
+    }
+    */
 
 
-        /*
-        GenomeWideList<MotifAnchor> l1 = MotifAnchorParser.loadMotifsFromGenomeID("hg19", null);
+    public static final double[][] points = {{1, 1}, {2, 1}, {1, 2},
+            {2, 2}, {3, 3}, {8, 8},
+            {9, 8}, {8, 9}, {9, 9}};
 
-        GenomeWideList<MotifAnchor> l2 = MotifAnchorParser.loadMotifsFromLocalFile(
-                "/Users/muhammadsaadshamim/Desktop/GenomeWideMotifs/motif_list_hg19_M1/M1_fimo_full_out_1M_t1e3/hg19.motifs.txt", "hg19", null);
+    public static void writePointsToFile(List<Vector> points,
+                                         String fileName,
+                                         FileSystem fs,
+                                         Configuration conf) throws IOException {
+        Path path = new Path(fileName);
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf,
+                path, LongWritable.class, VectorWritable.class);
+        long recNum = 0;
+        VectorWritable vec = new VectorWritable();
+        for (Vector point : points) {
+            vec.set(point);
+            writer.append(new LongWritable(recNum++), vec);
+        }
+        writer.close();
+    }
 
-        GenomeWideList<MotifAnchor> l3 = MotifAnchorParser.loadMotifsFromURL(
-                "http://hicfiles.s3.amazonaws.com/internal/motifs/hg19.motifs.txt", "hg19", null);
+    public static List<Vector> getPoints(double[][] raw) {
+        List<Vector> points = new ArrayList<Vector>();
+        for (int i = 0; i < raw.length; i++) {
+            double[] fr = raw[i];
+            Vector vec = new RandomAccessSparseVector(fr.length);
+            vec.assign(fr);
+            points.add(vec);
+        }
+        return points;
+    }
 
-        System.out.println(l1.size());
-        System.out.println(l2.size());
-        System.out.println(l3.size());
-        */
+    public static void main(String args[]) throws Exception {
 
+        int k = 2;
 
-/*
-        String[] ajkhsd = {"loop_domains",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_ld/loops.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_ld/domains.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_ld/ld.txt"};
+        List<Vector> vectors = getPoints(points);
 
-        //HiCGlobals.printVerboseComments = true;
-        //HiCTools.main(ajkhsd);
+        File testData = new File("testdata");
+        if (!testData.exists()) {
+            testData.mkdir();
+        }
 
+        testData = new File("testdata/points");
+        if (!testData.exists()) {
+            testData.mkdir();
+        }
 
-                /*
-        String[] ll51231123 = {"motifs",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/gm12878",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/loops_clean.txt",
-                "/Users/muhammadsaadshamim/Dropbox (Lab at Large)/GenomeWideMotifs/motif_list/REN_fimo_full_out_1M/fimo.txt"};
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        writePointsToFile(vectors, "testdata/points/file1", fs, conf);
 
-        //HiCGlobals.printVerboseComments = true;
-        HiCTools.main(ll51231123);
+        Path path = new Path("testdata/clusters/part-00000");
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf,
+                path, Text.class, Kluster.class);
 
-        String[] ll512123431123 = new String[]{"compare",
-                "0", "-m", "25000",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_aws_hiccups/geo.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_aws_hiccups/aws_loops_30.txt"};
+        for (int i = 0; i < k; i++) {
+            Vector vec = vectors.get(i);
+            Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
+            writer.append(new Text(cluster.getIdentifier()), cluster);
+        }
+        writer.close();
 
-        ll512123431123 = new String[]{"compare",
-                "-m", "5000", "1",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/original/new_suhas_list.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/loops_clean_with_motifs.txt"};
+        KMeansDriver.run(conf, new Path("testdata/points"), new Path("testdata/clusters"),
+                new Path("output"), 0.001, 10,
+                true, 0.001, false);
 
-        HiCTools.main(ll512123431123);
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs,
+                new Path("output/" + Cluster.CLUSTERED_POINTS_DIR
+                        + "/part-m-00000"), conf);
 
-        ll512123431123 = new String[]{"compare",
-                "-m", "5000", "2",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/original/new_suhas_list.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/loops_clean_with_motifs.txt"};
+        IntWritable key = new IntWritable();
+        WeightedVectorWritable value = new WeightedVectorWritable();
 
-        HiCTools.main(ll512123431123);
-
-        ll512123431123 = new String[]{"compare",
-                "-m", "5000", "2",
-                "hg19",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/loops_clean_with_motifs.txt",
-                "/Users/muhammadsaadshamim/Desktop/test/test_motifs/original/new_suhas_list.txt"};
-
-        HiCTools.main(ll512123431123);
-        */
+        while (reader.next(key, value)) {
+            System.out.println(value.toString() + " belongs to cluster "
+                    + key.toString());
+        }
+        reader.close();
     }
 }
